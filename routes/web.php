7321ad17ec2f89864e82aa2bd3b8adad4a546201
richue78 <62783870+richue78@users.noreply.github.com>
2022-02-15 10:58:05 +0700

    update lagi

--- routes/web.php
+++ routes/web.php
@@ -21,32 +21,23 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/success', function () {
-    return view('success');
-});
-
-Route::get('/logout', function () {
-    return view('logout');
-});
-
-
 Auth::routes();
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::get('/detail/{id}', [EbookController::class, 'show'])->middleware('auth');
 Route::post('/addToCart/{ebook}', [EbookController::class, 'addToCart'])->middleware('auth');
 Route::get('/cart/{user}', [EbookController::class, 'showCart'])->middleware('auth');
 Route::post('/cart/{order}', [EbookController::class, 'destroy'])->middleware('auth');
 Route::get('/submit', [EbookController::class, 'submit'])->middleware('auth');
-
 Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLang']);
-
 Route::get('/profile', [UserController::class, 'editProfile'])->middleware('auth');
 Route::post('/profile/edit', [UserController::class, 'updateProfile'])->middleware('auth');
-
+Route::get('/success', function () {
+    return view('success');
+});
+Route::get('/logout', function () {
+    return view('logout');
+});
 Route::get('/accountmaintenance', [UserController::class, 'maintenanceProfile'])->middleware('admin');
-
 Route::get('/updaterole/{id}', [UserController::class, 'updateRoleAccount'])->middleware('admin');
 Route::post('/updaterole/edit/{id}', [UserController::class, 'updateRoleById'])->middleware('admin');
 Route::post('/delete/{id}', [UserController::class, 'deleteRoleById'])->middleware('admin');
